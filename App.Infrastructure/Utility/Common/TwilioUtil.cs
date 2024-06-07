1a5acfade8d8cf1287d5affb387cb2b731921859
Xiasong Chen <296724771@qq.com>
2024-06-07 16:23:07 +0100

    fix: 注释无用代码

--- App.Infrastructure/Utility/Common/TwilioUtil.cs
+++ App.Infrastructure/Utility/Common/TwilioUtil.cs
@@ -23,24 +23,24 @@ namespace App.Infrastructure.Utility.Common
         public bool sendSMS(string phone, string content)
         {
 
-            try
-            {
-                string accountSid = "AC2edbf7ebba55ff47906bab408e8d5e1d";
-                string authToken = "5ded5c76459b4aa5363bac49b9b849fb";
+            //try
+            //{
+            //    string accountSid = "AC2edbf7ebba55ff47906bab408e8d5e1d";
+            //    string authToken = "5ded5c76459b4aa5363bac49b9b849fb";
 
-                TwilioClient.Init(accountSid, authToken);
+            //    TwilioClient.Init(accountSid, authToken);
 
 
-                var verification = VerificationResource.Create(
-                    to: "+353870647175",
-                    channel: "sms",
-                    pathServiceSid: "VA0d18e5e0b9b2f5846334087d6d692cab"
-                );
-            }
-            catch (Exception ex)
-            {
+            //    var verification = VerificationResource.Create(
+            //        to: "+8613928459050",
+            //        channel: "sms",
+            //        pathServiceSid: "VA0d18e5e0b9b2f5846334087d6d692cab"
+            //    );
+            //}
+            //catch (Exception ex)
+            //{
 
-            }
+            //}
 
             bool res = false;
 #if RELEASE
